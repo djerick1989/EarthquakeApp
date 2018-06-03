007069bd5a4352da6f916c1b91b49a0835c2d46b
Erick Martinez <erick.martinez@ccn.com.ni>
2018-06-02 23:01:06 -0600

    Agregando filtro de fechas al EventFragment

--- AndroidApp/app/src/main/java/test/nicaragua/com/earthquakeapp/EventFragment.java
+++ AndroidApp/app/src/main/java/test/nicaragua/com/earthquakeapp/EventFragment.java
@@ -1,23 +1,39 @@
 package test.nicaragua.com.earthquakeapp;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import test.nicaragua.com.earthquakeapp.database.EventRepository;
+import test.nicaragua.com.earthquakeapp.model.Event;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EventFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link EventFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class EventFragment extends Fragment {
+
+    List<Event> eventList;
+    Context mContext;
+    static Calendar mStarttime, mEndtime;
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     public EventFragment() {
         // Required empty public constructor
@@ -37,13 +53,61 @@ public class EventFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        eventList = new ArrayList<>();
+        mContext = getContext();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        mStarttime = Calendar.getInstance();
+        mStarttime.add(Calendar.DAY_OF_MONTH, -1);
+        mEndtime = Calendar.getInstance();
+
+        final Button btStartTime = (Button) view.findViewById(R.id.bt_starttime);
+        btStartTime.setText(df.format(mStarttime.getTime()));
+        btStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = mStarttime.get(Calendar.DAY_OF_MONTH);
+                int month = mStarttime.get(Calendar.MONTH);
+                int year = mStarttime.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mStarttime.set(year, month, dayOfMonth);
+                        btStartTime.setText(df.format(mStarttime.getTime()));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
+        final Button btEndTime = (Button) view.findViewById(R.id.bt_endTime);
+        btEndTime.setText(df.format(mEndtime.getTime()));
+        btEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = mEndtime.get(Calendar.DAY_OF_MONTH);
+                int month = mEndtime.get(Calendar.MONTH);
+                int year = mEndtime.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mEndtime.set(year, month, dayOfMonth);
+                        btEndTime.setText(df.format(mEndtime.getTime()));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        final DownloadTask downloadTask = new DownloadTask();
+        downloadTask.execute("");
+
+        return view;
     }
 
     @Override
@@ -56,18 +120,30 @@ public class EventFragment extends Fragment {
         super.onDetach();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+            try {
+                eventList = EventRepository.consumeWS(mContext, mStarttime.getTime(), mEndtime.getTime() );
+                data = "1";
+            } catch (Exception e) {
+                e.printStackTrace();
+                data = "-1";
+            }
+            return data;
+        }
+
+        // Una vez finalizada la descarga de datos
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result.equals("1")) {
+                // TODO: Load elements in RecyclerView
+            } else {
+                // TODO: Alert dialog
+            }
+        }
     }
+
 }
--- AndroidApp/app/src/main/java/test/nicaragua/com/earthquakeapp/MainActivity.java
+++ AndroidApp/app/src/main/java/test/nicaragua/com/earthquakeapp/MainActivity.java
@@ -1,7 +1,6 @@
 package test.nicaragua.com.earthquakeapp;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,20 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.Date;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
-import test.nicaragua.com.earthquakeapp.database.EventRepository;
-import test.nicaragua.com.earthquakeapp.model.Event;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Event> eventList;
+
     ViewPager viewPager;
     SlidePagerAdapter slidePagerAdapter;
-    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,66 +32,9 @@ public class MainActivity extends AppCompatActivity {
         slidePagerAdapter = new SlidePagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(slidePagerAdapter);
 
-        eventList = new ArrayList<>();
-        mContext = getBaseContext();
-
-        final DownloadTask downloadTask = new DownloadTask();
-        downloadTask.execute("");
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... url) {
-            String data = "";
-            try {
-                Calendar yesterday = Calendar.getInstance();
-                yesterday.add(Calendar.DAY_OF_MONTH, -1);
-                Calendar today = Calendar.getInstance();
-                Date starttime = yesterday.getTime();
-                Date endtime = today.getTime();
-                eventList = EventRepository.consumeWS(mContext, starttime, endtime );
-                data = "1";
-            } catch (Exception e) {
-                e.printStackTrace();
-                data = "-1";
-            }
-            return data;
-        }
-
-        // Una vez finalizada la descarga de datos
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (result.equals("1")) {
-                // TODO: Load elements in RecyclerView
-            } else {
-                // TODO: Alert dialog
-            }
-        }
-    }
 
     public class SlidePagerAdapter extends FragmentStatePagerAdapter {
 
